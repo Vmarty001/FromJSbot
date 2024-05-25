6576d574525992929092858af832aef2d23ba8ce
vmay <va>
2024-05-25 17:23:07 +0300

    fgfgd

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ bot.on('message', async (msg) => {
     }
 });
 
-app.post('/web-data', async (req, res) => {
+app.post('http://45.89.188.162:8000/web-data', async (req, res) => {
     const {queryId, products = [], totalPrice} = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
